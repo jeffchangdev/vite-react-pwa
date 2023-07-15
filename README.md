f0ef5d638d8a7821c96753a09bc1268ee9145d00
dukesilverest <dukesilverest@gmail.com>
2023-07-15 14:22:48 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Vite React PWA application for real time weather info by the hour 
---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+function App() {
+  return (
+    <div>Hello World. This is React + TypeScript + ESLint + Prettier.</div>
+  );
+}
+
+export default App;
